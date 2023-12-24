ca0f6503250128d0393a107cf51269926403d792
nafi-ullah <shafinnafiullah@gmail.com>
2023-12-24 18:54:29 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Cart from "./Cart";
 import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
+import Header from "./components/Header"
 
 const App = () => {
   const theme = {
@@ -42,6 +43,7 @@ const App = () => {
     <ThemeProvider theme={theme}>
     <Router>
       <GlobalStyle />
+      <Header />
         <Routes>
             <Route path="/" element= {<Home/>}> </Route>
             <Route path="/about" element= {<About/>}> </Route>
