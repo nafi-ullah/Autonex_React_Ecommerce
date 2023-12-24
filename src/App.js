34b861b655e9e9b013ae3db0b301b323ab998199
nafi-ullah <shafinnafiullah@gmail.com>
2023-12-24 15:16:22 +0600

    update other routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+import About from "./styles/About";
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
 
 const App = () => {
   return (
     <Router>
         <Routes>
             <Route path="/" element= {<Home/>}> </Route>
+            <Route path="/about" element= {<About/>}> </Route>
+            <Route path="/products" element= {<Products/>}> </Route>
+            <Route path="/contact" element= {<Contact/>}> </Route>
+            <Route path="/cart" element= {<Cart/>}> </Route>
           </Routes>
       </Router>
   );
