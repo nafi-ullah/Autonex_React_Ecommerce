fe0829a585cc3e1a1eaf6624a80ba466d7555d4a
nafi-ullah <shafinnafiullah@gmail.com>
2023-12-24 21:53:52 +0600

    home page updated

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,16 @@
 import React from "react";
-import styled from "styled-components";
+//import styled from "styled-components";
+import HeroSection from "./components/HeroSection";
+//import Dummy from "./components/Dummy";
 
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
+    const data = {
+        name: "AutoNex",
+      };
+    
+      return <HeroSection myData={data} />;
 };
 
-const Wrapper = styled.section`
-  height: 100vh;
-  background-color: ${({ theme }) => theme.colors.bg};
-`;
+
 
 export default Home;
