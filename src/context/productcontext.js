1f2037fb3f886e1d6c8d7ea045ddf91322545e99
nafi-ullah <shafinnafiullah@gmail.com>
2023-12-25 18:06:07 +0600

    useReducerhook fetch and manage data

--- src/context/productcontext.js
+++ src/context/productcontext.js
@@ -5,27 +5,49 @@
 // onno component theke abr seta import kore use korte hobe.
 
 
-import { createContext , useContext, useEffect} from "react";
+import { createContext , useContext, useEffect, useReducer} from "react";
 import axios from "axios";
+import reducer from "../reducer/productReducer";
 
 const AppContext = createContext();
 
 const API = "https://api.pujakaitem.com/api/products";
 
+const initialState ={
+    isLoading: false,
+    isError: false,
+    products: [],
+    featureProducts:[],
+};
+
+
 const AppProvider = ( {children}) => {  // kind of redux er moto statemanagement
 
+    //loading, error, data => 3ta initial state a data lagbe.
+    //userReducerhook 2ta element return kore arrayr
+    //goal hocche agy sob global store a sob data fetch kore thn ek ek component a fetch krbo
+    // intialstate er moddhe jei data ase, seta eshe porbe state a; kisu korte hole dispatch re bolte hobe
+    // dispatch prodcutReducer er action re bolbe kaaj ta korte. 
+    const [state, dispatch] = useReducer(reducer, initialState);
+
     const getProducts = async (url) => {
+        dispatch({ type: "SET_LOADING"});
+        try{
         const res = await axios.get(url);
         const products = await res.data;
-        console.log(" products ", products);
-        
+        // product jegula pabo, segula reducer hook er maddhome store korbo,
+        //console.log(" products ", products);
+        dispatch({type:"SET_API_DATA", payload: products}); // payload hocche dispatch je kaaj kortese seta korar jonno ki ki data lagbe
+        }catch(error){
+            dispatch({ type: "API_ERROR" });
+        }
     };
     useEffect(()=>{
         getProducts(API);
     }, []);
 
-
-        return (<AppContext.Provider value="autonex">  
+            //usereducer er sob data ekhane eshe porbe.
+        return (<AppContext.Provider value={{...state}}>  
             {children}
         </AppContext.Provider >
         );
---
+++ src/reducer/productReducer.js
@@ -0,0 +1,37 @@
+//Dispatch call korar por eke eke protita kaaj action re diye korabe. and type onujayi kaaj korbe.
+
+const ProductReducer = (state, action) => {
+    switch (action.type){
+        case "SET_LOADING":
+            return {
+                ... state,
+                isLoading: true
+            };
+         case "SET_API_DATA":
+            //payload hocche product gula jegula fetch kora hoise
+            const featureData = action.payload.filter((curElm)=>{
+                return curElm.featured === true;
+            });
+
+            return {
+                ... state,
+                isLoading: false,
+                products: action.payload,
+                featureProducts: [],
+            }
+
+        case "API_ERROR":
+            return {
+                    ... state,
+                    isLoading: true
+            };
+       
+        default:
+            return state;
+
+    }
+
+    return state;
+};
+
+export default ProductReducer;
