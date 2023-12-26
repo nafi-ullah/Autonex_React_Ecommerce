492469cbdd1062ec2fe02f782f353cd2efcb0e4d
nafi-ullah <shafinnafiullah@gmail.com>
2023-12-26 18:07:46 +0600

    product page and grid-list view

---
+++ src/reducer/filterReducer.js
@@ -0,0 +1,22 @@
+const filterReducer = (state, action) => {
+    switch (action.type) {
+      case "LOAD_FILTER_PRODUCTS":
+        return {
+          ...state,
+          filter_products: [...action.payload],
+          //... means original data use na kore oitar je copy seta use krtese
+          all_products: [...action.payload],
+        };
+  
+      case "SET_GRID_VIEW":
+        return {
+          ...state,
+          grid_view: true,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default filterReducer;
